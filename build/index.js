3d5593bb6a0cd7af03286393b6822e0931e9fa3b
ndxbxrme <lewis_the_cat@hotmail.com>
2017-01-22 11:17:17 +0000

    first commit

---
+++ build/index.js
@@ -0,0 +1,77 @@
+(function() {
+  'use strict';
+  module.exports = function(ndx) {
+    var FacebookStrategy, ObjectID;
+    FacebookStrategy = require('passport-facebook').Strategy;
+    ObjectID = require('bson-objectid');
+    ndx.settings.FACEBOOK_KEY = process.env.FACEBOOK_KEY || ndx.settings.FACEBOOK_KEY;
+    ndx.settings.FACEBOOK_SECRET = process.env.FACEBOOK_SECRET || ndx.settings.FACEBOOK_SECRET;
+    ndx.settings.FACEBOOK_CALLBACK = process.env.FACEBOOK_CALLBACK || ndx.settings.FACEBOOK_CALLBACK;
+    if (ndx.settings.FACEBOOK_KEY) {
+      ndx.passport.use(new FacebookStrategy({
+        clientID: ndx.settings.FACEBOOK_KEY,
+        clientSecret: ndx.settings.FACEBOOK_SECRET,
+        callbackURL: ndx.settings.FACEBOOK_CALLBACK,
+        passReqToCallback: true
+      }, function(req, token, refreshToken, profile, done) {
+        var newUser, users;
+        if (!req(user)) {
+          users = ndx.database.exec('SELECT * FROM ' + ndx.settings.USER_TABLE + ' WHERE facebook->id=?', [profile.id]);
+          if (users && users.length) {
+            if (!users[0].facebook.token) {
+              ndx.database.exec('UPDATE ' + ndx.settings.USER_TABLE + ' SET facebook=? WHERE _id=?', [
+                {
+                  token: token,
+                  name: profile.name.givenName + ' ' + profile.name.familyName,
+                  email: profile.emails[0].value
+                }, req.user._id
+              ]);
+              return done(null, users[0]);
+            }
+            return done(null, users[0]);
+          } else {
+            newUser = {
+              _id: ObjectID.generate(),
+              facebook: {
+                id: profile.id,
+                token: token,
+                name: profile.name.givenName + ' ' + profile.name.familyName,
+                email: profile.emails[0].value
+              }
+            };
+            ndx.database.exec('INSERT INTO ' + ndx.settings.USER_TABLE + ' VALUES ?', [newUser]);
+            return done(null, newUser);
+          }
+        } else {
+          ndx.database.exec('UPDATE ' + ndx.settings.USER_TABLE + ' SET facebook=? WHERE _id=?', [
+            {
+              id: profile.id,
+              token: token,
+              name: profile.name.givenName + ' ' + profile.name.familyName,
+              email: profile.emails[0].value
+            }, req.user._id
+          ]);
+          return done(null, req.user);
+        }
+      }));
+      ndx.app.get('/api/facebook', ndx.passport.authenticate('facebook', {
+        scope: 'email'
+      }), ndx.postAuthenticate);
+      ndx.app.get('/api/facebook/callback', ndx.passport.authenticate('facebook'), ndx.postAuthenticate);
+      ndx.app.get('/api/connect/facebook', ndx.passport.authorize('facebook', {
+        scope: 'email'
+      }));
+      return ndx.app.get('/api/unlink/facebook', function(req, res) {
+        var user;
+        user = req.user;
+        user.facebook.token = void 0;
+        user.save(function(err) {
+          res.redirect('/profile');
+        });
+      });
+    }
+  };
+
+}).call(this);
+
+//# sourceMappingURL=index.js.map
---
+++ src/index.coffee
@@ -0,0 +1,64 @@
+'use strict'
+
+module.exports = (ndx) ->
+  FacebookStrategy = require('passport-facebook').Strategy
+  ObjectID = require 'bson-objectid'
+  ndx.settings.FACEBOOK_KEY = process.env.FACEBOOK_KEY or ndx.settings.FACEBOOK_KEY
+  ndx.settings.FACEBOOK_SECRET = process.env.FACEBOOK_SECRET or ndx.settings.FACEBOOK_SECRET
+  ndx.settings.FACEBOOK_CALLBACK = process.env.FACEBOOK_CALLBACK or ndx.settings.FACEBOOK_CALLBACK
+  if ndx.settings.FACEBOOK_KEY
+    ndx.passport.use new FacebookStrategy
+      clientID: ndx.settings.FACEBOOK_KEY
+      clientSecret: ndx.settings.FACEBOOK_SECRET
+      callbackURL: ndx.settings.FACEBOOK_CALLBACK
+      passReqToCallback: true
+    , (req, token, refreshToken, profile, done) ->
+      if not req user
+        users = ndx.database.exec 'SELECT * FROM ' + ndx.settings.USER_TABLE + ' WHERE facebook->id=?', [profile.id]
+        if users and users.length
+          if not users[0].facebook.token
+            ndx.database.exec 'UPDATE ' + ndx.settings.USER_TABLE + ' SET facebook=? WHERE _id=?', [
+              {
+                token: token
+                name: profile.name.givenName + ' ' + profile.name.familyName
+                email: profile.emails[0].value
+              },
+              req.user._id
+            ]
+            return done null, users[0]
+          return done null, users[0]
+        else
+          newUser = 
+            _id: ObjectID.generate()
+            email: profile.emails[0].value
+            facebook:
+              id: profile.id
+              token: token
+              name: profile.name.givenName + ' ' + profile.name.familyName
+              email: profile.emails[0].value
+          ndx.database.exec 'INSERT INTO ' + ndx.settings.USER_TABLE + ' VALUES ?', [newUser]
+          return done null, newUser
+      else
+        ndx.database.exec 'UPDATE ' + ndx.settings.USER_TABLE + ' SET facebook=? WHERE _id=?', [
+          {
+            id: profile.id
+            token: token
+            name: profile.name.givenName + ' ' + profile.name.familyName
+            email: profile.emails[0].value
+          },
+          req.user._id
+        ]
+        return done null, req.user
+    ndx.app.get '/api/facebook', ndx.passport.authenticate('facebook', scope: 'email')
+    , ndx.postAuthenticate
+    ndx.app.get '/api/facebook/callback', ndx.passport.authenticate('facebook')
+    , ndx.postAuthenticate
+    ndx.app.get '/api/connect/facebook', ndx.passport.authorize('facebook',
+      scope: 'email')
+    ndx.app.get '/api/unlink/facebook', (req, res) ->
+      user = req.user
+      user.facebook.token = undefined
+      user.save (err) ->
+        res.redirect '/profile'
+        return
+      return
